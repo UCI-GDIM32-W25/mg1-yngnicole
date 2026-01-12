111e4ce6e2d2a9dfb48cffd55f2c27744cc1049f
yngnicole <nicoley5@uci.edu>
2026-01-12 00:17:34 -0800

    s

--- Assets/Scripts/PlantCountUI.cs
+++ Assets/Scripts/PlantCountUI.cs
@@ -9,10 +9,10 @@ public class PlantCountUI : MonoBehaviour
     public void UpdateSeeds (int seedsLeft, int seedsPlanted)
     {
         // Decreases the number of seeds the player has left, which is displayed in the UI.
-        _remainingText.text = "Seeds Remaining: " + seedsLeft;
+        _remainingText.text = "Seeds Remaining: " + seedsLeft--;
         
         // Increases the number of seeds the player has planted, which is displayed in the UI.
-        _plantedText.text = "Seeds Planted: " + seedsPlanted;
+        _plantedText.text = "Seeds Planted: " + seedsPlanted++;
 
     }
 }
