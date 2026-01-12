6c601aa64717fba25d5669731c849602cdf1015f
yngnicole <nicoley5@uci.edu>
2026-01-11 22:58:02 -0800

    wasd

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,11 +18,23 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        float movementX = Input.GetAxis("Horizontal");
+        float movementY = Input.GetAxis("Vertical");
         
+        Vector2 movement = new Vector2(movementX, movementY);
+
+        transform.Translate(movement * _speed * Time.deltaTime);
+        
+        //sprite movement WASD and/or arrow keys can be used to move the player.
+        //planting seed. instantiating SPACE key plants a seed, which: 
+        // needs to have amount of seeds
+        // Checks to see if the player has any seeds left. If they do, it performs actions 3-5; otherwise, nothing happens.
     }
 
     public void PlantSeed ()
     {
-        
+        //having a plant to call from 
+        // Makes a plant appear at the player’s location.
+        //
     }
 }
